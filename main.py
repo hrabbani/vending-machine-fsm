1477494663d640ee2e4d7835533fb62c63afa007
hrabbani <humza.rabbani@gmail.com>
2021-03-26 20:17:15 -0700

    missing comments + images

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def input_action(machine: VendingMachine):
         print("1. Refill\n2. Insert Coin\n3. Make Selection\n4. Cancel\n5. Quit\n")
         print("Current State: {}".format(machine.state))
         try:
-            action = int(input("What action would you like to take? (Enter the number)"))
+            action = int(input("What action would you like to take? (Enter the number): "))
 
             if action < 1 or action > 5:
                 print("Please enter a valid choice(1-5)")
--- vending_machine.py
+++ vending_machine.py
@@ -1,18 +1,23 @@
 import time
 
 class VendingMachine:
+
+    # defining the states
     STATE_IDLE = "Idle"
     STATE_WAITING = "Waiting"
     STATE_REFUNDING = "Refunding"
     STATE_DISPENSING = "Dispensing"
     
+
+    # defining the valid transitions
     VALID_TRANSITIONS = {
         STATE_IDLE: [STATE_WAITING, STATE_IDLE],
-        STATE_WAITING: [STATE_REFUNDING, STATE_DISPENSING],
+        STATE_WAITING: [STATE_REFUNDING, STATE_DISPENSING, STATE_WAITING],
         STATE_REFUNDING: [STATE_IDLE],
         STATE_DISPENSING: [STATE_REFUNDING, STATE_IDLE]
     }
     
+    # money inputs
     VALID_COINS = {
         "P": 0.01,
         "N": 0.05,
@@ -23,27 +28,42 @@ class VendingMachine:
         "10D": 10
     }
     
-    PRICE = 3
     
     def __init__(self): 
+
         self.state = self.STATE_IDLE
         self.items = {}
         self.balance = 0
+
+
         
-    def transition(self, new_state):
+    def transition(self, new_state): # will check if a transition is valid, then make the transition
+
+        # state is the same: valid transition, no need for print statement
+        if self.state == new_state: 
+            return True
         
-        if new_state in self.VALID_TRANSITIONS[self.state]:
+        # valid transition
+        if new_state in self.VALID_TRANSITIONS[self.state]: 
             self.state = new_state
+
 
             print("--------------------")
             print("State switched to {}\n--------------------".format(new_state))
+            return True
 
+        # invalid transition
         else: 
             print("Invalid transition: {} -> {}.".format(self.state, new_state))
-            return
+            return False
+            
 
         
-    def refill(self):
+    def refill(self): # will restore original quantities of all items
+
+        if not self.transition(self.STATE_IDLE):
+            return
+
         self.items = {
 
             "Pepsi": {
@@ -79,13 +99,17 @@ class VendingMachine:
         }
         print("--------------------")
         print("Machine refilled.")
+        print("--------------------")
+        time.sleep(1)
+
         
-        self.transition(self.STATE_IDLE)
         
-    def insert_coin(self):
-        if self.balance == 0 and self.state == self.STATE_IDLE: 
-            self.transition(self.STATE_WAITING)
-            
+        
+    def insert_coin(self): # user inserts coin into machine
+        if not self.transition(self.STATE_WAITING):
+            return 
+
+        # processing coin input    
         while True: 
             try: 
                 coin = input("Please add a coin to the machine (P, N, D, Q, 1D, 5D, 10D).")
@@ -105,7 +129,11 @@ class VendingMachine:
         print("Coin inserted. Your balance is {}".format(self.balance))
         print("----------------------------------------")
 
-    def make_selection(self):
+
+
+    def make_selection(self): # user selects item from machine
+
+        # checking if user has inserted money
         if self.state != self.STATE_WAITING: 
             print("--------------------")
             print("Invalid transition: You must insert money before making a selection.")
@@ -115,6 +143,7 @@ class VendingMachine:
 
         self.show_items()
 
+        # processing item input
         selected_item = None
         while True:
             try: 
@@ -141,13 +170,22 @@ class VendingMachine:
                 print("Please select a valid item.")
                 continue
 
+        # dispensing item and refunding change
         self.dispense(selected_item)
         time.sleep(1)
         self.process_refund(selected_item)
 
         
-    def process_refund(self, selected_item): 
+    def calc_refund(self, selected_item): # calculates refund amount based on price of item and current balance
+        price = self.items[selected_item]['price']
+        if self.balance < price:
+            return self.balance
+        return self.balance - price
+
+
+    def process_refund(self, selected_item): # refunds user, resets balance
         refund_amt = self.calc_refund(selected_item)
+        
         self.transition(self.STATE_REFUNDING)
 
         if refund_amt == 0: 
@@ -161,20 +199,16 @@ class VendingMachine:
             self.transition(self.STATE_IDLE)
 
 
-    def dispense(self, selected_item): 
+    def dispense(self, selected_item): # gives item to user, updates count of items in machine
         self.transition(self.STATE_DISPENSING)
         time.sleep(1)
         print("Purchasing {}...".format(selected_item))
         self.items[selected_item]['quantity'] -= 1      
 
 
-    def calc_refund(self, selected_item):
-        price = self.items[selected_item]['price']
-        if self.balance < price:
-            return self.balance
-        return self.balance - price
+    
 
-    def show_items(self):
+    def show_items(self): #displaying the menu of items in the machine
         print("--------------------\nCurrent Selection:")
         
         for item, info in self.items.items():
@@ -182,7 +216,7 @@ class VendingMachine:
         
         print("--------------------")
         
-    def cancel(self):
+    def cancel(self): # cancels transaction, refunds any money in machine
         if self.balance != 0: 
             self.transition(self.STATE_REFUNDING)
             time.sleep(1)
